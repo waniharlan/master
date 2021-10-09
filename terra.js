11cd3e96295f087970474bfe393b116dd707a8ed
waniharlan <86362041+waniharlan@users.noreply.github.com>
2021-10-09 19:51:06 +0700

    Create terra.js

---
+++ terra.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'f2c4c076eda87e29aa7fe2a65a0f7016',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectiveperformanceformat.com/f2c4c076eda87e29aa7fe2a65a0f7016/invoke.js"></scr' + 'ipt>');
+</script>
