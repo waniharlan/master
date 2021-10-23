663cfbb2ae2ff2c21d929d43d020418a498facf5
waniharlan <86362041+waniharlan@users.noreply.github.com>
2021-10-23 08:36:29 +0700

    Update terra.js

--- terra.js
+++ terra.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 	atOptions = {
 		'key' : 'f2c4c076eda87e29aa7fe2a65a0f7016',
 		'format' : 'iframe',
@@ -7,4 +7,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectiveperformanceformat.com/f2c4c076eda87e29aa7fe2a65a0f7016/invoke.js"></scr' + 'ipt>');
-</script>
+
